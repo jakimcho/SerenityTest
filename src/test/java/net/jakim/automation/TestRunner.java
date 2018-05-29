a45081cc93ecb49b4e6dad0a665fc5757de12a1e
Jakim <jakimcho@gmail.com>
2018-05-29 21:52:29 +0300

    added xray features dir

--- src/test/java/net/jakim/automation/TestRunner.java
+++ src/test/java/net/jakim/automation/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/login",
+        features = "src/test/resources/xray-features/",
         glue = "net.jakim.automation.steps.definition"
 )
 public class TestRunner {
