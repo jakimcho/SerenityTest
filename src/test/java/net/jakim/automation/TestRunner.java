f5744480c5ad8fa86ef029961c984c36e8021d16
Yakim H. Rachev <jakimcho@gmail.com>
2018-06-14 12:39:38 +0300

    Added Cucumber.json and junit.xml formatters

--- src/test/java/net/jakim/automation/TestRunner.java
+++ src/test/java/net/jakim/automation/TestRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        plugin = {"pretty" ,"html:Folder_Name" ,
-                "json:target/cucumber.json" ,
-                "junit:target/cucumber.xml"},
+        plugin =
+        {
+            "json:target/cucumber.json",
+            "junit:target/cucumber.xml"
+        },
         features = "xray-features/",
         //features = "src/test/resources/features",
         glue = "net.jakim.automation.steps.definition"
